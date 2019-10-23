be37ded228599b70ac3362792cfe532051ba916c
ws909 <and.o.jans@icloud.com>
2019-10-23 20:03:14 +0200

    Corrections

--- Project/build/scripts/container.js
+++ Project/build/scripts/container.js
@@ -1,17 +1,24 @@
 
-function waste(milliseconds){
-    var now = new Date().getTime();
-    while(new Date().getTime() < now + milliseconds) {}
-}
-
+import { elementExists, withFragmentID } from "./utilities.js";
 
 const contentContainer = document.querySelector("#content-container");
 const header = document.querySelector("#main-header");
 const footer = document.querySelector("#main-footer");
 
+function setURLQueryParameter(name, value) {
+    const urlParameters = new URLSearchParams(window.location.search);
+
+    if (urlParameters.get(name) == value) {
+        return;
+    }
+    urlParameters.set(name, value);
+
+    window.history.replaceState({content: window.location.href}, "", decodeURIComponent(`${location.pathname}?${urlParameters}`));
+}
+
 /// requires same-origin of the IFrame content. Local file access is considered cross-origin. Deactivate local file restrictions for debugging purposes.
 function scaleIFrame() {
-    height = contentContainer.contentWindow.document.documentElement.offsetHeight
+    var height = contentContainer.contentWindow.document.documentElement.offsetHeight
     height = Math.max(height, window.innerHeight - header.offsetHeight - footer.offsetHeight);
 
     contentContainer.style.height = height + "px";
@@ -27,19 +34,18 @@ function scaleIFrame() {
 }
 
 function setContentSource(url) {
+    if (elementExists(window.location.hash)) {
+        const url = withFragmentID(url, "");
+    }
+
     document.body.scrollTo(0, 0);
     contentContainer.src = url;
 
-    // url = new URL(sender.dataset.url, document.URL);
-    //url = document.URL + "#" + sender.dataset.url.slice(0, -4);
-
-    window.location.hash = url.slice(0, -5);
-
-    //window.history.pushState("", "", url);
+    setURLQueryParameter("content", url);
 }
 
-function setContent(sender) {
-    setContentSource(sender.dataset.url);
+function setContent() {
+    setContentSource(this.dataset.url);
 }
 
 function contentDidLoad() {
@@ -56,10 +62,16 @@ function setContentContainerOffset() {
     contentContainer.style.paddingTop = header.offsetHeight + "px";
 }
 
-fragmentID = window.location.hash;
+contentContainer.addEventListener("load", contentDidLoad);
 
-if (fragmentID.length !== 0) {
-    setContentSource(fragmentID.slice(1) + ".html");
+document.querySelectorAll(".header-button").forEach(button => {
+    button.addEventListener("click", setContent);
+});
+
+const contentSpecifier = new URLSearchParams(window.location.search).get("content");
+
+if (contentSpecifier) {
+    setContentSource(contentSpecifier);
 }
 
 setContentContainerOffset();
---
+++ Project/build/scripts/utilities.js
@@ -0,0 +1,35 @@
+
+// No reason keeping this for the final version
+export function waste(milliseconds) {
+    const now = new Date().getTime();
+    while (new Date().getTime() < now + milliseconds) {}
+}
+
+
+export function withFragmentID(url, id) {
+    if (id === null) {
+        return url;
+    }
+
+    const i1 = url.indexOf("#");
+
+    if (id === "") {
+        return (i1 != -1) ? url.slice(0, i1) : url;
+    }
+
+    if (i1 != -1) {
+        return url.slice(0, i1 + 1) + id;
+    } else {
+        return url + "#" + id;
+    }
+
+    return url;
+}
+
+export function elementExists(selector) {
+    if (selector === null || typeof selector !== "string" || selector === "") {
+        return false;
+    }
+
+    return document.querySelector(selector) != null;
+}
