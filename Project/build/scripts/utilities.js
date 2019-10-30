1c335af84f74fb73862959d55779695c1a334878
ws909 <and.o.jans@icloud.com>
2019-10-30 23:35:21 +0100

    ...

--- Project/build/scripts/utilities.js
+++ Project/build/scripts/utilities.js
@@ -174,7 +174,7 @@ async function f() {
             console.log(json);
         })
 
-        txt = await load("txt", "file.txt")
+        txt = await load("txt", "file.txt");
         console.log(txt);
 
         dom = await load("DOM", "file.html", (result) => {
