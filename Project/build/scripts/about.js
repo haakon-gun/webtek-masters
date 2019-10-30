12cc58bf937d896d53786721ca741972249b1145
Haakon Gunnarsli <haakgu@stud.ntnu.no>
2019-10-30 02:49:18 +0100

    Implemented information popup box for all employees.

--- Project/build/scripts/about.js
+++ Project/build/scripts/about.js
@@ -1,67 +1,178 @@
 
 employees = {
-    mai: {
+    employee0: {
       name: "Mai Gunnarsli",
       title: "Tannlege",
       image: "resources/mai-kersten-gunnarsli.png",
+      experiences: [
+      "Født i Oslo, flyttet til Skien som 13 åring.",
+      "Utdannet tannlege ved universitetet i Bergen i 1986.",
+      "Allmenntannlege med spesiell interesse for krone-broprotetikk og implantatprotetikk.",
+      "1987-89: Avtjent førstegangstjeneste som tannlege i forsvaret ved Haakonsvern, videre som garnisonstannlege på Akershus festning.",
+      "1990-1997: Jobbet som assistenttannlege i privatpraksis i Skien.1997- Egen privatpraksis i Skien.",
+      "2011 Autorisasjon som tannlege med rett til å utføre implantatprotetisk behandling med offentlig økonomisk støtte.",
+      "Fra 2001 fulgt Tannlegeforeningens Systematiske Etterutdanning"
+      ]
     },
-    kirsten: {
+    employee1: {
       name: "Kirsten Benedikte Aanesen",
       title: "Tannlege",
-      image: "resources/kirsten-b-aanesen.png",},
-    kari: {
+      image: "resources/kirsten-b-aanesen.png",
+      experiences:[
+      "Født og oppvokst i Skien.",
+      "Er uteksaminert tannlege fra Universitetet i Oslo i 1984.",
+      "Allmenntannlege med spesiell interesse for krone-broprotetikk og estetisk tannbehandling.",
+      "1984-85 Militærtannlege på Gardermoen Flystasjon.",
+      "1985-87 Privat praksis Lillestrøm.",
+      "1987-91 Assistenttannlege Skien hos Tannlege Gunnar Snartland1991- Egen privatpraksis Skien.",
+      "Har fulgt og gjennomført alle fagområdene i Tannlegeforeningens Systematiske Etterutdanning (TSE) fra 2001."
+      ]
+    },
+    employee2: {
       name: "Kari Nesse",
       title: "Tannlege",
-      image: "resources/kari-nesse.png",},
-    christine: {
+      image: "resources/kari-nesse.png",
+      experiences:[
+      "Født og oppvokst i Porsgrunn.",
+      "Utdannet tannlege i Oslo 1981.",
+      "Allmenntannlege med spesiell interesse for implantatprotetikk, kirurgi og søvnapne behandling.",
+      "1981-1985 : Distriktstannlege på Ørnes i Nordland.",
+      "1985-1989 : Deltidsstilling som skoletannlege ,vikar Skien, Siljan og Brevik.",
+      "1985-1989: Deltidsstilling privat praksis.Fra 1989 : Egen privat praksis i Skien.",
+      "2010: Autorisasjon som tannlege med rett til å utføre implantatprotetisk behandling med offentlig økonomisk støtte.",
+      "2012: Diplomgodkjenning med tillatelse til å behandle med SomnoDent søvnapneskinner.",
+      "Fra 2001 fulgt Tannlegeforeningens Systematiske Etterutdannelse."
+      ]
+    },
+    employee3: {
       name: "Christine Aanesen",
       title: "Tannlege",
-      image: "resources/christine-aanesen.png",},
-    hanne: {
+      image: "resources/christine-aanesen.png",
+      experiences:[
+      "Født og oppvokst i Porsgrunn.",
+      "Er uteksaminert tannlege fra Universitetet i Bergen 2015.",
+      "Kurs i bleking av tenner."
+      ]
+    },
+    employee4: {
       name: "Hanne Hauge Hommedal",
       title: "Tannpleier",
-      image: "resources/hanne-hommedal.png",},
-    erna: {
+      image: "resources/hanne-hommedal.png",
+      experiences:[
+      "Født og oppvokst i Bergen",
+      "Utdannet tannpleier på Universitet i Bergen i 2010",
+      "Jobbet i den offentlige tannhelsetjenesten i over 8 år",
+      "Jobber 1dag i uken på Ibsen Tannlegesenter."
+      ]
+    },
+    employee5: {
       name: "Erna Oterholt Johnsen",
       title: "Tannhelsesekretær",
-      image: "resources/erna-oterholt.png",},
-    anne: {
+      image: "resources/erna-oterholt.png",
+      experiences:[
+      "Født og oppvokst i Skien.",
+      "Jobbet i privat tannlegepraksis i Oslo fra 1975-1982",
+      "Flyttet til Skien i 1982 og har jobbet i privat praksis siden.",
+      "Har vært tannhelsesekretær hos tannlege Mai Kersten Gunnarsli siden 1997."
+      ]
+    },
+    employee6: {
       name: "Anne Kari Johansen",
       title: "Tannhelsesekretær",
-      image: "resources/anne-kari-johansen.png",},
-    marlene: {
+      image: "resources/anne-kari-johansen.png",
+      experiences:[
+      "Født i Porsgrunn og oppvokst i Skien",
+      "Sekretær i Skien Boligbyggelag i 8 år",
+      "Autorisasjon som tannhelsesekretær i 2008",
+      "Jobbet som tannhelsesekretær hos Kirsten Aanesen siden 1993"
+      ]
+    },
+    employee7: {
       name: "Marlene Almli",
       title: "Tannhelsesekretær",
-      image: "resources/marlene-S-Almli.png",},
-    signe: {
+      image: "resources/marlene-S-Almli.png",
+      experiences:[
+      "Født og oppvokst i Skien.",
+      "Utdannet tannhelsesekretær i 2005 ved Holmestrand videregående skole.",
+      "Fra 2005 jobbet som tannhelsesekretær i privat praksis."
+      ]
+    },
+    employee8: {
       name: "Signe Lie",
       title: "Tannhelsesekretær",
-      image: "resources/Signe-lie.png",}
+      image: "resources/Signe-lie.png",
+      experiences:[
+      "Født og oppvokst i Porsgrunn",
+      "Jobbet ved Ibsen Tannlegesenter siden 2016"
+      ]
+    }
 }
 
 
 gridContainer = document.querySelector(".grid-container");
-console.log("hei")
 
-for (employe in employees) {
-  createEmployeDiv(employees[employe]);
-  console.log("hei2");
+for (employee in employees) {
+  createEmployeDiv(employees[employee]);
 }
 
-function createEmployeDiv(employe){
+function createEmployeDiv(employee){
   let node = document.createElement("div");
   node.className = "employees";
   let img = document.createElement("img");
-  img.src = employe.image;
+  img.src = employee.image;
   img.className = "img";
+  img.onclick = function() {populateEmployeeContent(employee); modal.style.display = "block";}
   let name = document.createElement("p");
   name.className = "name";
-  name.appendChild(document.createTextNode(employe.name));
+  name.onclick = function() {modal.style.display = "block";}
+  name.appendChild(document.createTextNode(employee.name));
   let title = document.createElement("p");
   title.className = "title";
-  title.appendChild(document.createTextNode(employe.title))
+  title.onclick = function() {modal.style.display = "block";}
+  title.appendChild(document.createTextNode(employee.title))
   node.appendChild(img);
   node.appendChild(name);
   node.appendChild(title);
   gridContainer.appendChild(node);
+}
+
+
+let employeesExperienceImg = document.querySelector("#employeesExperienceImg");
+let employeesExperienceName = document.querySelector("#employeesExperienceName");
+let employeesExperienceTitle = document.querySelector("#employeesExperienceTitle");
+let employeesExperienceList = document.querySelector("#employeesExperienceList");
+console.log(employeesExperienceName);
+
+
+function populateEmployeeContent(employee){
+  employeesExperienceImg.src = employee.image;
+  employeesExperienceName.innerHTML = employee.name;
+  employeesExperienceTitle.innerHTML = employee.title;
+  while(employeesExperienceList.firstChild){
+  employeesExperienceList.removeChild(employeesExperienceList.firstChild );
+  }
+  for (var i = 0; i<employee["experiences"].length; i++) {
+      console.log("hei")
+      let li = document.createElement("li");
+      li.appendChild(document.createTextNode(employee.experiences[i]));
+      employeesExperienceList.appendChild(li);
+    }
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
