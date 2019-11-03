f19fd40c6c3efcef8228f34ed11e7ba0228de282
Per Olav Stryken Haug <perolavhaug@hotmail.com>
2019-11-03 20:42:17 +0100

    litt front

---
+++ Project/build/scripts/slideshow.js
@@ -0,0 +1,21 @@
+
+var i = 0;
+var images = [];
+var time = 3300;
+
+images[0] = 'resources/stol.png';
+images[1] = "resources/favicon2.png";
+images[3] = 'resources/dntf.png';
+
+function changeImg(){
+  document.slideshow.src = images[i];
+
+  if(i < images.length - 1){
+    i++;
+  } else{
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+wibdow.onload = changeImg;
