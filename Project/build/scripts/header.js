716dd10b972e8fc789a8835a190f9f53877ae4e8
Trym Kulseng <trymkul@gmail.com>
2019-11-06 13:50:27 +0100

    animasjon header

--- Project/build/scripts/header.js
+++ Project/build/scripts/header.js
@@ -10,16 +10,16 @@ document.write('\
           </span>\
       </a>\
 \
-      <a class="header-link" href="about.html">\
+      <a class="header-link" id="link1" href="about.html">\
           <span><p>Om oss</p></span>\
       </a>\
-      <a class="header-link" href="treatments.html">\
+      <a class="header-link" id="link2" href="treatments.html">\
           <span><p>Behandlinger</p></span>\
       </a>\
-      <a class="header-link" href="prices.html">\
+      <a class="header-link" id="link3" href="prices.html">\
           <span><p>Priser</p></span>\
       </a>\
-      <a class="header-link" href="contact.html">\
+      <a class="header-link" id="link4" href="contact.html">\
           <span><p>Kontakt</p></span>\
       </a>\
   </nav>\
@@ -28,13 +28,21 @@ document.write('\
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("website-header").style.height ="80px"
-    document.getElementById("website-header").style.fontSize ="100%"
-    console.log("pess")
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("website-header").style.height = "80px";
+    document.getElementById("website-header").style.fontSize = "100%";
+    document.getElementById("headerLogo").style.marginTop = "3px";
+    document.getElementById("link1").style.height = "100%";
+    document.getElementById("link2").style.height = "100%";
+    document.getElementById("link3").style.height = "100%";
+    document.getElementById("link4").style.height = "100%";
   } else {
-    document.getElementById("website-header").style.width ="100%"
-    document.getElementById("website-header").style.height = "180px"
-    document.getElementById("headerLogo").style.width = "200px"
+    document.getElementById("website-header").style.width = "100%";
+    document.getElementById("website-header").style.height = "180px";
+    document.getElementById("headerLogo").style.marginTop = "60px";
+    document.getElementById("link1").style.height = "75%";
+    document.getElementById("link2").style.height = "75%";
+    document.getElementById("link3").style.height = "75%";
+    document.getElementById("link4").style.height = "75%";
   }
 }
