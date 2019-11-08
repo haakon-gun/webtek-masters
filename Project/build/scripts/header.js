ab1150f34412760fc308ee5d411cebc54f3ae878
ThomasEilifsen <thomnei@stud.ntnu.no>
2019-11-08 13:48:07 +0100

    fiksa bug i header animation

--- Project/build/scripts/header.js
+++ Project/build/scripts/header.js
@@ -53,6 +53,7 @@ function scrollFunction() {
     document.getElementById("website-header").style.width = "100%";
     document.getElementById("website-header").style.height = "80px";
     document.getElementById("headerLogo").style.width = "200px";
+    document.getElementById("headerLogo").style.marginTop = "3px";
     document.getElementById("link1").style.height = "75%";
     document.getElementById("link2").style.height = "75%";
     document.getElementById("link3").style.height = "75%";
