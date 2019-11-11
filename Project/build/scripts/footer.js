3e10fb0064b489322d4330976a7c94a27eb4e3cd
ThomasEilifsen <thomnei@stud.ntnu.no>
2019-11-11 17:21:04 +0100

    fjerna unødvendig css link

--- Project/build/scripts/footer.js
+++ Project/build/scripts/footer.js
@@ -67,37 +67,3 @@ function createFooter(){
 
 }
 createFooter()
-/*
-document.write(`
-<div id="website-footer">
-    <div id="footer-box-map">
-        <iframe id="map" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJWdJJ6SwhR0YRa2D-z285A2E&key=AIzaSyBITdjEW51MyztEZQGDNFcxuz1YZP_KUlk" allowfullscreen></iframe>
-    </div>
-
-    <div class="footer-text">
-        <div id="line1">
-            <h4>Adresse</h1>
-            <p>Henrik Ibsens gate 6: 3. etasje. 3724 Skien</p>
-            <a href="tel:+4735520469">Telefon: 35 52 04 69</a>
-        </div>
-        <div id="line2">
-            <h4>Åpningstider</h1>
-            <p>08:00–15:00, mandag til torsdag</p>
-            <p>08:00–13:00, fredag</p>
-        </div>
-        <div id="line3">
-            <h4>Grenland tannlegevakt</h4>
-            <p>Helger og helligdager: 10:00–17:00</p>
-            <a href="tel:+4790913377">Telefon: 90 91 33 77</a>
-        </div>
-
-        <div id="line4">
-            <a href="https://www.tannlegeforeningen.no">
-                <img alt="Den norske tannlegeforeningen" src="resources/dntf.png">
-                <h4 class="website-footer-heading">Den norske tannlegeforeningen</h1>
-            </a>
-        </div>
-    </div>
-</div>
-`);
-*/
